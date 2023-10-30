d0b4051fc1c4c9bbc40d106086ddb982542547ec
Brian Milian <brianmilian@csu.fullerton.edu>
2023-10-30 03:14:08 -0700

    Initial Commit

---
+++ ToDo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo App
+//
+//  Created by csuftitan on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
