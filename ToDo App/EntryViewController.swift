1c829a6b282428356b7e0ee7886a20c603019ab8
brian-250 <brianmilian@csu.fullerton.edu>
2023-10-31 13:35:22 -0700

    Add delete button which is still in progress

--- ToDo App/EntryViewController.swift
+++ ToDo App/EntryViewController.swift
@@ -7,23 +7,53 @@
 
 import UIKit
 
-class EntryViewController: UIViewController {
+class EntryViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var field: UITextField!
+    
+    // 
+    var update: (() -> Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        field.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveTask))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        saveTask()
+        
+        return true
     }
-    */
+    
+    @objc func saveTask() {
+        
+        // Checking to see if the field if NOT empty
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+        // Guarding against count being "nil" or empty or nothing
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        // Increment counter by 1 to say that we have more things in our tasks
+        let newCount = count + 1
+        
+        // Set new count again
+        UserDefaults().set(newCount, forKey: "count")
+        // Save our tasks by calling 'text', saving our new count
+        // each time we save this new task, it will be unique
+        UserDefaults().set(text, forKey: "task_\(newCount)")
+        
+        // if this update function exists, lets call it
+        update?()
+        
+        // Once we called update, lets dismiss this viewcontroller
+        navigationController?.popViewController(animated: true)
+    }
 
 }
--- ToDo App/ViewController.swift
+++ ToDo App/ViewController.swift
@@ -8,12 +8,99 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "Tasks"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Setup
+        // setup initial saving mechanism
+        if !UserDefaults().bool(forKey: "setup") {
+            // setup initial default (by default we have zero tasks)
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+        }
+
+        
+        // Get all current saved tasks
+        updateTasks()
     }
-
-
+    
+    // We recreate task array
+    func updateTasks() {
+        
+        // Before we update, removes all the elements in tasks array to not showe duplicates
+        tasks.removeAll()
+        
+        // Get count
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        // For loop to interate through zero up until count(inclusive of count)-we are going to get each of our tasks and add it to our tasks array
+        for x in 0..<count {
+            
+            // Add tasks to tasks array
+            // we start at zero, we never actually use zero because we add one to it every time we add
+            // if this task we pull out isn't empty, we are going to add it to our tasks array
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String {
+                tasks.append(task)
+            }
+        }
+        // We also want to reload out table view
+        tableView.reloadData()
+    }
+    
+    @IBAction func didTapAdd() {
+        
+        let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
+        vc.title = "New Task"
+        // We want to call this function in this class
+        vc.update = {
+            // We want to make sure that we prioritize updating the actual tasks (by calling the updateTasks function)
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+                
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
+
+
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView( _ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        vc.task = tasks[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView( _ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
+}
